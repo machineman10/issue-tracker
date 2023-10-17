3ab50de0fb37c7a481c60f23f98b935c10d09b04
shuvo <shuvorj38@gmail.com>
2023-10-17 12:36:26 +0600

    styled active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,24 +1,38 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+
   return (
     <nav className="flex border-b mb-5 px-5 h-12 items-center gap-6">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
+      <h1>
+        <Link href="/">
+          <AiFillBug />
+        </Link>
+      </h1>
       <ul className="flex gap-6">
-        <li>
-          <Link href="/" className="navlink">
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link href="/issues" className="navlink">
-            Issues
-          </Link>
-        </li>
+        {links.map((link, i) => (
+          <li
+            key={i}
+            className={classNames({
+              navlink: true,
+              active: link.href === currentPath,
+            })}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
