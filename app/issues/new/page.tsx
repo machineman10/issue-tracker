a5a83364f1b26a1c84eb63447941c65c23d5635b
shuvo <shuvorj38@gmail.com>
2023-10-17 23:04:09 +0600

    handle issue form submit error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
-import { Button, TextArea, TextFieldInput } from "@radix-ui/themes";
+import {
+  Button,
+  CalloutIcon,
+  CalloutRoot,
+  CalloutText,
+  TextArea,
+  TextFieldInput,
+} from "@radix-ui/themes";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface IssueForm {
@@ -12,19 +21,34 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const { register, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const onSubmit: SubmitHandler<IssueForm> = async (data) => {
-    await axios.post("/api/issues", data);
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("Something went wrong!");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-xl space-y-3">
-      <TextFieldInput placeholder="Title" {...register("title")} />
-      <TextArea placeholder="Description" {...register("description")} />
-      <Button type="submit">Submit new issue</Button>
-    </form>
+    <div className="max-w-xl space-y-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+        <TextFieldInput placeholder="Title" {...register("title")} />
+        <TextArea placeholder="Description" {...register("description")} />
+        <Button type="submit">Submit new issue</Button>
+      </form>
+      {error && (
+        <CalloutRoot color="red">
+          <CalloutIcon>
+            <InfoCircledIcon />
+          </CalloutIcon>
+          <CalloutText>{error}</CalloutText>
+        </CalloutRoot>
+      )}
+    </div>
   );
 };
 
