038159b0c388fff50dd14b0e36a49c6efbaa293a
shuvo <shuvorj38@gmail.com>
2023-10-20 19:40:10 +0600

    show a confirm delete modal

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,45 @@
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Flex,
+} from "@radix-ui/themes";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      <p>Delete issue</p>
-    </Button>
+    <AlertDialogRoot>
+      <AlertDialogTrigger>
+        <Button color="red">
+          <TrashIcon />
+          <p>Delete issue</p>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+        <AlertDialogDescription>
+          Do you want to permanently delete this issue?
+        </AlertDialogDescription>
+
+        <Flex mt="4" gap="3" justify="end">
+          <AlertDialogCancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </Flex>
+      </AlertDialogContent>
+    </AlertDialogRoot>
   );
 };
 
