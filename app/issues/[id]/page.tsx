ab7bbf2fb3a139a24a7a39cd6ce662e169c15ef7
shuvo <shuvorj38@gmail.com>
2023-10-20 19:27:41 +0600

    add delete issue button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import { Box, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 
@@ -14,13 +15,18 @@ const IssueDetailsPage = async ({
   if (!issue) notFound();
 
   return (
-    <Flex direction={{ initial: "column", md: "row" }} gap="5">
+    <Flex direction={{ initial: "column" }} gap="4">
       <Box>
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <EditIssueButton issueId={issue.id} />
-      </Box>
+      <Flex gap="4">
+        <Box>
+          <EditIssueButton issueId={issue.id} />
+        </Box>
+        <Box>
+          <DeleteIssueButton issueId={issue.id} />
+        </Box>
+      </Flex>
     </Flex>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+import { Container, Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@radix-ui/themes/styles.css";
-import "./theme-config.css";
-import "./globals.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
+import "./globals.css";
+import "./theme-config.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme accentColor="violet" radius="small">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
