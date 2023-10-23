93a52272c635b2f6400fb488b522e976f10acf8c
shuvo <shuvorj38@gmail.com>
2023-10-23 12:04:18 +0600

    implement pagination logic

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -5,6 +7,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   totalItem: number;
@@ -13,14 +16,33 @@ interface Props {
 }
 
 const Pagination = ({ totalItem, itemPerPage, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const totalPage = Math.ceil(totalItem / itemPerPage);
+
+  const goToPage = (pageIndex: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageIndex.toString());
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <Flex align="center" gap="2">
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => goToPage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => goToPage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
 
@@ -28,10 +50,20 @@ const Pagination = ({ totalItem, itemPerPage, currentPage }: Props) => {
         Page {currentPage} of {totalPage}
       </Text>
 
-      <Button color="gray" variant="soft" disabled={currentPage === totalPage}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === totalPage}
+        onClick={() => goToPage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === totalPage}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === totalPage}
+        onClick={() => goToPage(totalPage)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Pagination } from "./components";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <main>
       <h1>Home page</h1>
-      <Pagination totalItem={100} itemPerPage={10} currentPage={5} />
+      <Pagination
+        totalItem={100}
+        itemPerPage={10}
+        currentPage={+searchParams.page || 1}
+      />
     </main>
   );
 }
